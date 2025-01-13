2915b82e2b3cea39654dfd19904938b75e05e0d2
Ashen Madhushanka <161613675+amliyanage@users.noreply.github.com>
2025-01-13 14:34:38 +0530

    Add dashboard layout and navigation components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LoginPage from "./pages/LoginPage.tsx";
 import RegisterPage from "./pages/RegisterPage.tsx";
 import {ToastContainer} from "react-toastify";
+import RootLayout from "./components/RootLayout.tsx";
+import Home from "./components/Home.tsx";
 
 function App() {
 
@@ -13,6 +15,10 @@ function App() {
             <Routes>
                 <Route path="/" element={<LoginPage/>} />
                 <Route path="/register" element={<RegisterPage />} />
+
+                <Route path="/dashbaord" element={ <RootLayout /> } >
+                    <Route path="home" element={ <Home /> } />
+                </Route>
             </Routes>
           </BrowserRouter>
       </>
