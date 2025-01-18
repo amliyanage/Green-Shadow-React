1e9ea2142a0450c6b85a5d66ad2dbd5be6ef7a8f
Ashen Madhushanka <161613675+amliyanage@users.noreply.github.com>
2025-01-18 16:37:57 +0530

    Add UpdateEquPopup component and integrate with EquWall for equipment updates

--- src/components/wall/EquWall.tsx
+++ src/components/wall/EquWall.tsx
@@ -5,16 +5,24 @@ import SaveEquPopup from "../popups/Equ/SaveEquPopup.tsx";
 import {useSelector} from "react-redux";
 import {convertEquArrayTo2DArray} from "../../util/ArrayTo2DArray.ts";
 import {Equ} from "../../model/Equ.ts";
+import UpdateEquPopup from "../popups/Equ/UpdateEquPopup.tsx";
 
 const EquWall = () => {
     const dataHeaders = [ "equipment Id" , "Name" , "equipment Type" , "status"]
     const [savePopup, setSavePopup] = useState(false)
+    const [updatePopup, setUpdatePopup] = useState(false)
     const [search, setSearch] = useState("")
     const equData = useSelector((state: {equ:Equ[]}) => state.equ)
     const [filteredData, setFilteredData] = useState(convertEquArrayTo2DArray(equData))
+    const [targetEqu, setTargetEqu] = useState("")
 
     const handleSavePopup = () => {
         setSavePopup(!savePopup)
+    }
+
+    const handleUpdatePopup = (id:string) => {
+        setUpdatePopup(!updatePopup)
+        setTargetEqu(id)
     }
 
     useEffect(() => {
@@ -32,9 +40,10 @@ const EquWall = () => {
     return(
         <>
             {savePopup && <SaveEquPopup closePopupAction={handleSavePopup} />}
+            {updatePopup && <UpdateEquPopup closePopupAction={handleUpdatePopup} targetEqu={targetEqu} />}
             <div className="w-100 p-5 bg-transparent" id="staff-wall">
                 <WallHeader title={"Equipment Management"} addPopupAction={handleSavePopup} searchAction={setSearch}/>
-                <Table headersData={dataHeaders} bodyData={filteredData} />
+                <Table headersData={dataHeaders} bodyData={filteredData} updatePopupAction={handleUpdatePopup} />
             </div>
         </>
     )
